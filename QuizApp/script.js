dd48f13eaf0a898c72978979fb2039e65e2d3bec
Archit Tiwary <tiwaryart007@gmail.com>
2026-09-10 11:24:44 +0000

    proj2

---
+++ QuizApp/script.js
@@ -0,0 +1,143 @@
+const question = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers: [
+            { text: "Earth", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Venus", correct: false },
+        ]
+    },
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Berlin", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Madrid", correct: false },
+        ]
+    },
+    {
+        question: "How many continents are there in the world?",
+        answers: [
+            { text: "5", correct: false },
+            { text: "6", correct: false },
+            { text: "7", correct: true },
+            { text: "8", correct: false },
+        ]
+    },
+    {
+        question: "Which is the fastest land animal?",
+        answers: [
+            { text: "Lion", correct: false },
+            { text: "Cheetah", correct: true },
+            { text: "Horse", correct: false },
+            { text: "Tiger", correct: false },
+        ]
+    },
+    {
+        question: "Which gas do humans need to breathe to survive?",
+        answers: [
+            { text: "Carbon Dioxide", correct: false },
+            { text: "Nitrogen", correct: false },
+            { text: "Oxygen", correct: true },
+            { text: "Hydrogen", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;//reset
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < question.length) {
+        handleNextButton();
+    }
+    else {
+        startQuiz();
+    }
+})
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${question.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < question.length) {
+        showQuestion();
+    }
+    else {
+        showScore();
+    }
+}
+
+
+startQuiz();
